b0af9be58a198add1ef31bb9045c062a7b7c913b
kusch ivan <i-kush@mail.ru>
2021-12-09 10:31:02 +0700

    Комбинированная сортировка Notebook

---
+++ src/main/java/lesson2/SpeedTest.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class SpeedTest {
+
+    private static Long time;
+
+    public static void startTime() {
+        time = System.currentTimeMillis();
+    }
+
+    public static void endTime() {
+        time = System.currentTimeMillis() - time;
+        System.out.println(time);
+    }
+}
