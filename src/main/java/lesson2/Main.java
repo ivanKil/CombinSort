f300e537c688b89f1c5bab3d2984e51dfaeadc3d
kusch ivan <i-kush@mail.ru>
2021-12-16 12:46:00 +0700

    Поиск пропущенного числа

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
         Notebook[] arr = Notebook.getArray(SIZE);
         //System.out.println(Arrays.toString(arr));
-        SpeedTest.startTime();
-        CombSort.sort(arr);
-        SpeedTest.endTime();
+        //SpeedTest.startTime();
+        //CombSort.sort(arr);
+        BinarySearch.searchSkippedNum(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16});
+        //BinarySearch.search(new Integer[]{1, 2, 3, 5});
+        //BinarySearch.search(new Integer[]{});
+        //SpeedTest.endTime();
         //System.out.println(Arrays.toString(arr));
     }
 }
